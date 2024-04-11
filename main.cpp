292b8a007e1b7170a827ae9ea2dac5291d003b50
Demis Getachew <demisgech@gmail.com>
2024-04-11 16:19:25 +0300

    Provide a function for a salary calulator object that was blotted main function.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,10 @@
 #include <iostream>
 #include <iomanip>
-#include "salaryCalculator.cpp"
+#include "utils/manageEmployee.cpp"
 using namespace std;
 
 int main()
 {
 
-    Salary salary[3] = {};
-    for (int i = 0; i < 3; i++)
-        salary[i].receiveData();
-
-    for (int i = 0; i < 3; i++)
-        salary[i].calculateSalary();
-    // for (int i = 0; i < 3; i++)
-    //     salary[i].displayData();
-
-    cout << "Search Employee by id:" << endl;
-    int id;
-    cin >> id;
-    for (int i = 0; i < 3; i++)
-        if (salary[i].employee.id == id)
-            cout << "Id" << setw(10) << "Name" << setw(10) << "NetSalary" << endl
-                 << salary[i].employee.id << setw(12) << salary[i].employee.name << setw(10) << salary[i].netSalary << endl;
     return 0;
 }
---
+++ utils/manageEmployee.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <iomanip>
+#include "salaryCalculator.cpp"
+using namespace std;
+
+void manageEmployee()
+{
+    Salary salary[3] = {};
+    for (int i = 0; i < 3; i++)
+        salary[i].receiveData();
+
+    for (int i = 0; i < 3; i++)
+        salary[i].calculateSalary();
+    // for (int i = 0; i < 3; i++)
+    //     salary[i].displayData();
+
+    cout << "Search Employee by id:" << endl;
+    int id;
+    cin >> id;
+    for (int i = 0; i < 3; i++)
+        if (salary[i].employee.id == id)
+            cout << "Id" << setw(10) << "Name" << setw(10) << "NetSalary" << endl
+                 << salary[i].employee.id << setw(12) << salary[i].employee.name << setw(10) << salary[i].netSalary << endl;
+}
