b4e123f21878a9d590f4e4066118104a6f7ea6a1
Demis Getachew <demisgech@gmail.com>
2024-04-11 16:00:06 +0300

    Remove *.cpp and *.h file from external folder.

--- forSum.cpp
+++
@@ -1,9 +0,0 @@
-int sum(int n)
-{
-    int sum = 0;
-    if (n <= 0)
-        return n;
-    for (int i = 1; i <= n; i++)
-        sum += i;
-    return sum;
-}
--- recursionSum.cpp
+++
@@ -1,6 +0,0 @@
-int recursionSum(int n)
-{
-    if (n <= 0)
-        return n;
-    return (n + recursionSum(n - 1));
-}
